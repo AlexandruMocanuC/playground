63d4454a547dea7748c1cfb659da90704abe0f29
Alex Mocanu <mocanu.alexandru20@yahoo.ro>
2022-01-15 20:01:49 +0200

    Card 2: Created component for 3 columns layout

---
+++ src/components/Forest/Columns3.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Columns3 = ({ columns }) => {
+	return (
+		<div className="columns-3">
+			{columns.map((column, index) => (
+				<div className="column" key={index}>
+					<div
+						className="bg-cover preview"
+						style={{
+							backgroundImage: column.image,
+						}}
+					></div>
+					<div className="caption">
+						<p>{column.description}</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Columns3;
