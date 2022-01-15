04d54dba849475e4cb65899e57084c54574640e7
Alex Mocanu <mocanu.alexandru20@yahoo.ro>
2022-01-15 21:32:21 +0200

    Card 9: Added db to files

--- src/components/Forest/Columns3.js
+++ src/components/Forest/Columns3.js
@@ -4,20 +4,28 @@ const Columns3 = ({ columns }) => {
 	return (
 		<div className="columns-3">
 			{columns.map((column, index) => (
-				<div className="column" key={index}>
-					<div
-						className="bg-cover preview"
-						style={{
-							backgroundImage: column.image,
-						}}
-					></div>
-					<div className="caption">
-						<p>{column.caption}</p>
-					</div>
-				</div>
+				<Column
+					image={column.image}
+					caption={column.caption}
+					key={index}
+				/>
 			))}
 		</div>
 	);
 };
+
+const Column = ({ image, caption }) => (
+	<div className="column">
+		<div
+			className="bg-cover preview"
+			style={{
+				backgroundImage: image,
+			}}
+		></div>
+		<div className="caption">
+			<p>{caption}</p>
+		</div>
+	</div>
+);
 
 export default Columns3;
